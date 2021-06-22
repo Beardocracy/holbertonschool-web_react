90f3ecb41c465a740583ffa9719e281404d2d977
Beardocracy <travisjbearden@gmail.com>
2021-06-22 20:36:01 +0000

    Added requested files

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,21 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const studentA: Student = {
+    firstName: 'First',
+    lastName: 'Student',
+    age: 1,
+    location: 'Tulsa'
+}
+
+const studentB: Student = {
+    firstName: 'Second',
+    lastName: 'Student',
+    age: 2,
+    location: 'Flat Earth'
+}
+
