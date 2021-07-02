2bfe6a416ab7e169d7ff8f73dc46f9375ed6072d
Beardocracy <travisjbearden@gmail.com>
2021-07-02 11:09:46 -0500

    Added files from task1

---
+++ 0x02-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,25 @@
+import logo from './holberton_logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+        
+      </header>
+      <hr />
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <hr />
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
