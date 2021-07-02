a9fe856f78c5f36427562e30c49f879c14ed1d74
Beardocracy <travisjbearden@gmail.com>
2021-07-02 11:15:15 -0500

    Added form

--- 0x02-react_intro/task_2/dashboard/src/App.js
+++ 0x02-react_intro/task_2/dashboard/src/App.js
@@ -8,11 +8,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
-        
       </header>
       <hr />
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <form>
+          <label>
+            Email:
+            <input type="text" name="email"/>
+          </label>
+          <label>
+            Password
+            <input type="text" name="password"/>
+          </label>
+          <button>OK</button>
+        </form>
       </div>
       <hr />
       <div className="App-footer">
